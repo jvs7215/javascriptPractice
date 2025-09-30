508da6c7e7247abeffd9c45fef283e7a098fc56b
Jessica Salemme <jvs7215@psu.edu>
2025-09-30 14:59:02 -0400

    digit 400 exercise1

---
+++ javascriptExercise1.js
@@ -0,0 +1,23 @@
+window.addEventListener('DOMContentLoaded',init,false);
+            
+function init() {
+    alert('The page loaded!');
+}
+
+window.addEventListener('DOMContentLoaded',init,false);
+
+function init() {
+    alert ('Check if page loaded');
+
+   var buttons = document.getElementsByTagName("button")
+buttons[0].addEventListener('click', changeColor,false)
+}
+
+function changeColor() {
+alert('The page loaded!')}
+/* here, style is a *property*: the CSS styling of an element: you can add a CSS property after invoking style. */
+
+function newFunction() { 
+/* We haven't written what is supposed to happen here yet. But this is an example of a JavaScript comment. */
+}
+     
